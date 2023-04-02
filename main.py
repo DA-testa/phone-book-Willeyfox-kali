53b08fe0c07e59d88e2522ad539e7ac61aedf437
Oleksandr Voznenko <124575056+Willeyfox-kali@users.noreply.github.com>
2023-04-03 00:14:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
-
+class Contacts:
+    def __init__(self, name):
+        self.name = name
+        
 def read_queries():
     n = int(input())
     queries = []
@@ -17,37 +20,24 @@ def read_queries():
         queries.append(query)
     return queries
 
-
 def write_responses(result):
     print('\n'.join(result))
 
-
 def process_queries(queries):
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
     contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for i in contacts:
-                if i.number == cur_query.number:
-                    contacts.remove(i)
-                    break
+    for query in queries:
+        if query.type == 'add':
+            contacts[query.number] = Contact(query.name)
+        elif query.type == 'del':
+            if query.number in contacts:
+                del contacts[query.number]
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            response = contacts.get(query.number)
+            if response:
+                result.append(response.name)
+            else:
+                result.append('not found')
     return result
 
 def main():
